bcfc879f4751e994660a52a155e8f1a20a58ce61
Md Anik Hossain <anikh499@gmail.com>
2023-04-10 01:19:13 +0600

    today work finish

--- src/Components/Share/Footer/Footer.js
+++ src/Components/Share/Footer/Footer.js
@@ -23,30 +23,30 @@ const Footer = () => {
             <img src={twitter} alt="twitter" />
             <img src={linkedIn} alt="LinkedIn" />
             <img src={insta} alt="Instagram" /> */}
-            <a href="#facebook" className="facebookBtn m-2 w-10 h-10 inline-flex justify-center items-center shadow-sm rounded-full border-2 " style={{ borderColor: "#ffffff", color: "#ffffff" }}>
+            <Link to="#facebook" className="facebookBtn m-2 w-10 h-10 inline-flex justify-center items-center shadow-sm rounded-full border-2 " style={{ borderColor: "#ffffff", color: "#ffffff" }}>
               {/* ::facebook svg */}
               <svg className="w-6 h-6 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
                 <path d="M16.403,9H14V7c0-1.032,0.084-1.682,1.563-1.682h0.868c0.552,0,1-0.448,1-1V3.064c0-0.523-0.401-0.97-0.923-1.005C15.904,2.018,15.299,1.999,14.693,2C11.98,2,10,3.657,10,6.699V9H8c-0.552,0-1,0.448-1,1v2c0,0.552,0.448,1,1,1l2-0.001V21c0,0.552,0.448,1,1,1h2c0.552,0,1-0.448,1-1v-8.003l2.174-0.001c0.508,0,0.935-0.381,0.993-0.886l0.229-1.996C17.465,9.521,17.001,9,16.403,9z" />
               </svg>
-            </a>
+            </Link>
 
 
             {/* :Twitter */}
-            <a href="#twitter" className="twitterBtn m-2 w-10 h-10 inline-flex justify-center items-center shadow-sm rounded-full border-2" style={{ borderColor: "#ffffff", color: "#ffffff" }}>
+            <Link to="#twitter" className="twitterBtn m-2 w-10 h-10 inline-flex justify-center items-center shadow-sm rounded-full border-2" style={{ borderColor: "#ffffff", color: "#ffffff" }}>
               {/* ::twitter svg */}
               <svg className="w-5 h-5 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
                 <path d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z" />
               </svg>
-            </a>
+            </Link>
             {/* :Linkedin */}
-            <a href="#linkedin" className="linkedInBtn m-2 w-10 h-10 inline-flex justify-center items-center shadow-sm rounded-full border-2" style={{ borderColor: "#ffffff", color: "#ffffff" }}>
+            <Link to="#linkedin" className="linkedInBtn m-2 w-10 h-10 inline-flex justify-center items-center shadow-sm rounded-full border-2" style={{ borderColor: "#ffffff", color: "#ffffff" }}>
               {/* ::linkedin svg */}
               <svg className="w-5 h-5 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
                 <path d="M4.98 3.5c0 1.381-1.11 2.5-2.48 2.5s-2.48-1.119-2.48-2.5c0-1.38 1.11-2.5 2.48-2.5s2.48 1.12 2.48 2.5zm.02 4.5h-5v16h5v-16zm7.982 0h-4.968v16h4.969v-8.399c0-4.67 6.029-5.052 6.029 0v8.399h4.988v-10.131c0-7.88-8.922-7.593-11.018-3.714v-2.155z" />
               </svg>
-            </a>
+            </Link>
             {/* :Instagram */}
-            <a href="#instagrap" className="InstaBtn border-2 relative m-2 p-0.5 w-10 h-10 inline-flex justify-center items-center shadow-sm rounded-full bg-transparent text-white">
+            <Link to="#instagrap" className="InstaBtn border-2 relative m-2 p-0.5 w-10 h-10 inline-flex justify-center items-center shadow-sm rounded-full bg-transparent text-white">
               {/* ::instagram svg */}
               <div className=" relative z-10 w-full h-full rounded-full flex justify-center items-center ">
                 <svg className="w-5 h-5 text-white fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
@@ -55,7 +55,7 @@ const Footer = () => {
               </div>
               {/* ::border gradient */}
               <div className="absolute inset-0 w-full h-full rounded-full bg-gradient-to-tr " />
-            </a>
+            </Link>
 
           </div>
 
